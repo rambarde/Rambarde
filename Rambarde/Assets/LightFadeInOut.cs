d67849982e1197554aacc46fe535e51520b6c121
Amir BHK <amirbenhajkhaled@gmail.com>
2020-05-15 15:11:57 +0200

    Fix Flickering Light

---
+++ Rambarde/Assets/LightFadeInOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class LightFadeInOut : MonoBehaviour
+{
+    public Light light;
+    public float speed;
+    private Sequence FadeInOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+       FadeInOut = DOTween.Sequence();
+       FadeInOut.Append(light.DOIntensity(2.8f, speed));
+       FadeInOut.Append(light.DOIntensity(2.2f, speed));
+       FadeInOut.SetLoops(-1);
+       FadeInOut.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
